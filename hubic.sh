a1b3edd199ddfaf0adcce770b6871578321507ee
Corentin DERET <c.deret@hotmail.fr>
2016-04-08 11:45:14 +0200

    Update hubic.sh
    add condition for exesting files

--- hubic.sh
+++ hubic.sh
@@ -6,9 +6,21 @@
 set pass "YOUR_PASSWORD"
 account "YOUR_EMAIL"
 
+
+FILE=""
+
+# Put here your HubiC Folder 
+DIR="/"
+
+# look for empty dir
+# If not start synchro :
+
+if [ "$(ls -A $DIR)" ]; then
+     echo "$DIR is not Empty"
+     
 hubic stop
 
-# Prevent if "Cannot contact daemon, are you sure it is running?" warning.
+# Prevent if "Cannot contact daemon, are you sure it is running?" warning exists.
 export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --session --fork --print-address`
 
 hubic start
@@ -22,5 +34,11 @@ send "$pass\r"
 # Be sure that you've got directory in your /
 
 touch /loghubic/"$(date +%F) done"
+
+else
+    # We add log notification
+    touch /loghubic/"$(date +%F) $DIR is Empty"
+fi
+# rest of the logic
 
 exit
